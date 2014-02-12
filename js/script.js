17d32006477912fc6e0e90b692645a2e9ba75ab9
schurter <mtortillas@aol.com>
2014-02-12 12:50:40 -0500

    updated for the gracy group

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
-// JavaScript Document
-onload=init;
-
-function init() {
-	//make an object based on the Drop Class:
-	var drop1 = new Drop();
-	drop1.create();
-}
-//let's make a Class (blueprint) for each Drop we generate
+/**
+ * The Drop class is a blueprint for each raindrop we generate
+ * @author  John Doe
+ * @version 1.0, May 2014
+ */
 function Drop(){
-	this.x; //starts empty, will keep track of each Drop's left-right position
-	this.y; //starts empty, will keep track of each Drop's up-down position
-	this.item_on_page;
-	/** function does lots of things when a Drop gets created on the page
-	*
+	this.x; //starts empty, will keep track of each drop's left-right position as a #
+	this.y; //starts empty, will keep track of each drop's up-down position as a #
+	this.item_on_page; //represents drop's physical presence on the screen
+	/** 
+	*	The create method does lots of things when a drop gets created on the page
 	*/
 	this.create = function(){
-		//make a section element in the HTML, store it into the item-on-page we set up above.
+		//make an <img> tag in the HTML, store it into the item-on-page we set up above.
 		this.item_on_page = document.createElement("section");
-		//store a random x and y position, different for each Drop. I'm using screen width or 500, height of 300:
+		this.item_on_page.className = "raindrop";
+		//store a random x and y position, different for each drop. I'm using screen width or 500, height of 300:
 		this.x = Math.floor(Math.random()*500);
 		this.y = Math.floor(Math.random()*300);
-		//use those x and y coordinates in the CSS to position the Drops:
+		//use those x and y coordinates in the CSS to position the drops:
 		this.item_on_page.style.left = this.x + "px";
 		this.item_on_page.style.top = this.y + "px";
 		//attach the item to our HTML hierarchy, as a child of the body:
 		document.getElementsByTagName("body")[0].appendChild(this.item_on_page);
 	}
-	/** function does lots of things when a Drop is removed from the page
-	*
+	/** 
+	*   The destroy function does lots of things when a drop is removed from the page
 	*/
 	this.destroy = function(){
 		
 	}
-} //close the Class
+} //close the Drop class
+
+
+onload=init;
+
+function init() {
+	//make an object that's an instance of the Drop Class:
+	var drop1 = new Drop();
+	drop1.create();
+}
